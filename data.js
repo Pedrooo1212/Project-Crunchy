671a96f847ef9c7345679c002478bbbc1f2a7e37
unknown <pedrokc7@gmail.com>
2024-04-01 11:18:22 -0300

    iniciando projeto

---
+++ data.js
@@ -0,0 +1,64 @@
+const animes = [
+    {
+        nome: 'Dragon Ball',
+        nota: 4.7,
+        resenhas: 51
+
+    },
+    {
+        nome: 'JUJUTSU KAISEN',
+        nota: 4.9,
+        resenhas: 358
+    },
+    {
+        nome: 'Naruto',
+        nota: 4.6,
+        resenhas: 289
+    },
+    {
+        nome: 'One Pice',
+        nota: 4.9,
+        resenhas: 429
+    },
+    {
+        nome: 'Naruto Shippuden',
+        nota: 4.7,
+        resenhas: 392
+    },
+    {
+        nome: 'Demon Slayer: Kimetsu no Yaiba',
+        nota: 4.8,
+        resenhas: 559
+    },
+    {
+        nome: 'Nisekoi',
+        nota: 4.6,
+        resenhas: 28
+    },
+    {
+        nome: 'Dragon Ball Z',
+        nota: 4.8,
+        resenhas: 537
+    },
+    {
+        nome: 'Dragon Ball Super',
+        nota: 4.9,
+        resenhas: 519
+    },
+    {
+        nome: 'Dragon Ball GT',
+        nota: 4.2,
+        resenhas: 56
+    },
+    {
+        nome: 'One Punch-man',
+        nota: 4.6,
+        resenhas: 102
+    },
+    {
+        nome: 'Death Note',
+        nota: 4.8,
+        resenhas: 42
+    }
+
+]
