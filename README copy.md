2c677dde12192d30d753ffb44b3cded0ffa7ccfc
Alberto <betim009@gmail.com>
2024-04-04 18:57:45 -0300

    novas tasks

---
+++ README copy.md
@@ -0,0 +1,37 @@
+# Project-Crunchy
+
+## Check-list / lista de tarefas / lista de estudos
+
+### Git e GitHub
+
+Utilizamos git através do terminal:
+
+1. Comandos: `git add .` é utilizado para adicionar as novas atualizações do projeto.
+2. Comando: `git commit -m "o que foi alterado do projeto"` é utilizado para descrever o que foi alterado no projeto.
+3. Comando `git push` utilzado para enviar para o github.
+4. Comando `git pull` utilizado para atualizar do remoto para o desktop.
+
+### Checklist projeto
+
+* [X]  Orientar através da imagem task-img-1
+* [X]  Os mais populares serão os de maiores notas
+* [X]  Categorias de gênero definidas
+* [X]  Começar a construção do HTML para preparar as categorias
+* [X]  Começar a construção do JS para colocar os dados dentro do HTML
+* [X]  Terminar a categoria de Ação
+* [ ]  Terminar a categoria de Aventura
+* [X]  Melhorar e corrigir a base de dados, URL e IMG
+* [X]  Praticar comandos git
+* [ ]  Criar mais 3 novas Paginas (ação/populares/aventura): acao.html, populares.html, aventura.html
+* [ ]  Instalar bootstrap para as 3 paginas
+* [ ]  Criar arquivos javascript para as 3 paginas: acao.js, populares.js, aventura.js
+* [ ]  Conectar/linkar cada arquivo HTML e JS
+* [ ]  As novas paginas DEVEM trazer para elas o arquivo data.js
+* [ ]  As paginas vão exibir todos os animes que conrespondem ao teu próprio GENERO
+* [ ]  A pagina index.html deve ter um link para navegar para as outras páginas(acao.html, populares.html, aventura.html ( tag a )
+
+### CheckList de JS e HTML e CSS
+
+* [ ]  Links e Documentações
+* [ ]  Bootstrap
+* [ ]  Filter</a>
--- src/script.js
+++ src/script.js
@@ -1,20 +1,20 @@
-// precisamos resgatar os dados do arquivo data.js
+// 1 passo - resgatar a estrutura de dados data.js [x]
 import { animes } from "./data.js";
 
-// precisamos resgatar o elemento HTML para colocar os dados dentro
-const divPopulares = document.getElementById('div-populares');
+// 2 passo - resgartar elementos HTML 
+const divPopulares = document.getElementById('div-populares'); // 3 passo divPopulares
 const divAcao = document.getElementById('div-acao')
 
-// o seguinte simbolo => se chama arrow function, ele dispara uma ação!  
+// 4 passo - Criar uma nova estrutura para colocar dentro do HTML e DIV  
 const dataPopulares = animes.filter(element => element.nota >= 4.8).filter((el, index) => index < 5);
+// 5 passo - dataPopulares foi feito com uma estrutura de repetição
+// chamada filter, que filtrou 5 animes com notas maiores que 4.8
+
 const dataAcao = animes.filter(element => element.genero == "Ação")
-console.log(dataAcao)
 
-
-// IREMOS focar em fazer através de uma estrutura de REPETIÇÃO
-
-// COLOCAMOS dentro de uma DIV os DADOS(data)
-
+// 6 passo - Criamos uma estrutura de repetição, for of
+// Para criar elementos dentro da divPopulares
+// Criamos h3, p e uma img para a divPopulares
 for (const element of dataPopulares) {
     divPopulares.innerHTML += `
         <div class="col-sm-12 col-md-6 col-lg">
@@ -29,10 +29,10 @@ for (const element of dataPopulares) {
 for (const element of dataAcao) {
     divAcao.innerHTML += `
     <div class="col-sm-12 col-md-6 col-lg">
-    <h3>${element.nome}</h3>
-    <p>${element.genero} ${element.resenhas} avaçoações</p>
-    <img class="img-fluid" src="${element.img}">
-    <a href="${element.url}" target="_blank" class="btn btn-warning fw bold">assista</a>
+        <h3>${element.nome}</h3>
+        <p>${element.genero} ${element.resenhas} avaçoações</p>
+        <img class="img-fluid" src="${element.img}">
+        <a href="${element.url}" target="_blank" class="btn btn-warning fw bold">assista</a>
     </div>
     `
 }
