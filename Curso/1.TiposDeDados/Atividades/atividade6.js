d87d0d7afecb9ae0051bc591684aab457f53eab3
Pedrooo1212 <165676622+Pedrooo1212@users.noreply.github.com>
2024-05-08 15:39:13 -0300

    Atividades feitas

--- Curso/1.TiposDeDados/Atividades/atividade6.js
+++ Curso/1.TiposDeDados/Atividades/atividade6.js
@@ -13,3 +13,6 @@ const arrNumeros = [
     2
 ];
 
+const total = [arrNumeros[0] + arrNumeros[1] + arrNumeros[2] + arrNumeros[3] + arrNumeros[4] + arrNumeros[5]]
+
+console.log(total)
--- Curso/1.TiposDeDados/Atividades/script7.js
+++ Curso/1.TiposDeDados/Atividades/script7.js
@@ -8,3 +8,10 @@
 const prova = [
     10, 8, 9, 3, 10, 5, 2, 1, 5, 6, 10, 5, 5, 5, 5, 10
 ];
+
+const media = prova[0] + prova[1] + prova[2] + prova[3] + prova[15] 
+const media2 = prova[5] + prova[6] + prova[7] + prova[8] + prova[14]
+const media3 = prova[9] + prova[10] + prova[11] + prova[12] + prova[13] 
+
+console.log("a media final da prova e:")
+console.log(media + media2 + media3 / 15) 
