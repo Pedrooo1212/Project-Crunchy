63629458e589817c8488adba5422eed20e4cda25
Alberto <betim009@gmail.com>
2024-05-17 18:56:14 -0300

    atividades

---
+++ Curso/3.Repeticao/script3-Element.js
@@ -0,0 +1,9 @@
+const array = ["Azul", "Preto", "Vermelho"];
+
+for (let index = 0; index < array.length; index++) {
+    console.log(array[index]);
+    console.log('-------------');
+    const element = array[index];
+    console.log(element);
+    console.log('---------------');
+};
